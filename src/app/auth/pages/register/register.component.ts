60c9a38d80de07316d7d289a7560c044ecc4c497
Puntonatural <116592883+Puntonatural@users.noreply.github.com>
2023-06-08 16:32:53 -0500

    BANCOS LIST

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -27,17 +27,49 @@ export class RegisterComponent implements OnInit{
                 munId: 0,
                 cliBanco: '',
                 cliTipoCuenta: '',
-                cliNumCuenta: ''
+                cliNumCuenta: '',
+                userIdentificacion:''
               }
   
-    banco:any=[{
-                id: 1,
-                nombre: 'Bancolombia'
-                },
-                {
-                  id: 2,
-                  nombre: 'Davivienda'
-                  }
+    banco:any=[{ id: 1,nombre: 'BAN100' },
+               { id: 2,nombre: 'BANCAMIA S.A.' },
+               { id: 2,nombre: 'BANCO AGRARIO' },
+               { id: 2,nombre: 'BANCO AV VILLAS' },
+               { id: 2,nombre: 'BANCO BBVA COLOMBIA S.A.' },
+               { id: 2,nombre: 'BANCO CAJA SOCIAL' },
+               { id: 2,nombre: 'BANCO COPERATIVO COOPCENTRAL' },
+               { id: 2,nombre: 'BANCO DAVIVIENDA' },
+               { id: 2,nombre: 'BANCO DE BOGOTA' },
+               { id: 2,nombre: 'BANCO DE OCCIDENTE' },
+               { id: 2,nombre: 'BANCO FALABELLA' },
+               { id: 2,nombre: 'BANCO FINANDINA S.A. BIC' },
+               { id: 2,nombre: 'BANCO GNB SUDAMERIS' },
+               { id: 2,nombre: 'BANCO ITAU' },
+               { id: 2,nombre: 'BANCO PICHINCHA S.A.' },
+               { id: 2,nombre: 'BANCO POPULAR' },
+               { id: 2,nombre: 'BANCO SANTANDER COLOMBIA ' },
+               { id: 2,nombre: 'BANCO SERFINANZA ' },
+               { id: 2,nombre: 'BANCO UNION antes GIROS' },
+               { id: 2,nombre: 'BANCOLOMBIA ' },
+               { id: 2,nombre: 'BANCOOMEVA S.A. ' },
+               { id: 2,nombre: 'CFA COOPERATIVA FINANCIERA ' },
+               { id: 2,nombre: 'CITIBANK' },
+               { id: 2,nombre: 'COLTEFINANCIERA' },
+               { id: 2,nombre: 'CONFIAR COOPERATIVA FINANCIERA' },
+               { id: 2,nombre: 'COOFINEP COOPERATIVA FINANCIERA' },
+               { id: 2,nombre: 'COTRAFA' },
+               { id: 2,nombre: 'DALE' },
+               { id: 2,nombre: 'DAVIPLATA' },
+               { id: 2,nombre: 'IRIS' },
+               { id: 2,nombre: 'LULO BANK' },
+               { id: 2,nombre: 'MOVII.S.A.' },
+               { id: 2,nombre: 'NEQUI' },
+               { id: 2,nombre: 'RAPPIPAY' },
+               { id: 2,nombre: 'SCOTIABANK COLPATRIA' }
+
+
+                 
+
     ]
     tipoCuenta:any=[{
                       id: 1,
@@ -79,7 +111,8 @@ export class RegisterComponent implements OnInit{
   miFormularioRegistro: FormGroup = this.fb.group(
     {
       nombreReg: ['', [Validators.required, Validators.minLength(4)]],
-      emailReg: ['', [Validators.required, Validators.email]],
+      emailReg: ['', [ Validators.email]],
+      identReg: ['', [Validators.required]],
       passwordReg: ['', [Validators.required, Validators.minLength(6)]],
       convenioReg: ['', [Validators.required]],
       bancoReg: ['', [Validators.required]],
@@ -114,10 +147,11 @@ export class RegisterComponent implements OnInit{
  
   registrar(){
 
-        console.log("registrando", this.miFormularioRegistro.value.municipioReg.munId);
+        console.log("registrando", this.miFormularioRegistro.value.identReg);
       
         this.user.username = this.miFormularioRegistro.value.emailReg;
         this.user.email =  this.miFormularioRegistro.value.nombreReg;
+        this.user.userIdentificacion = this.miFormularioRegistro.value.identReg;
         this.user.password = this.miFormularioRegistro.value.passwordReg;
         this.user.convId = Number(this.miFormularioRegistro.value.convenioReg.covId);
         this.user.munId = Number(this.miFormularioRegistro.value.municipioReg.munId);
